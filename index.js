75d6f750ce18cb0403e893765d51f49ebc47215a
hasansaad1 <hasansaad.saad1998@gmail.com>
2017-09-30 22:26:03 +0300

    minor

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function myAnimate(){
     document.getElementById("myPic").removeEventListener("onclick",myAnimate);
     var arr = document.getElementsByClassName("hidden");
     for (var i=0;i<arr.length;i++){
+        arr[i].style.display = "block";
         arr[i].className += " show";
     }
     document.getElementById("myPic").className += " shrinked";
